3af5cef40c703822ff0ab25ffb3b383063364bca
itggot-william-sjoberg <william.sjoberg@itggot.se>
2017-03-10 12:18:26 +0100

    gues the number
    new guess the number

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -2,34 +2,51 @@ running = true
 easy = rand 1..10
 medium =rand 1..50
 hard = rand 1..100
-guesses = 0
+tries = 0
 
 while running
   puts "welcome to geuss a number"
   puts "choose dificulty"
   puts "easy,medium,hard"
-  gets.chomp = input
+  input = gets.chomp
   if input == easy
     puts "guess a number betwen 1 and 10"
-    gets.chomp = svar1
-    if svar1 == easy
-      running = false
-      p "congrats"
-    else
-      gesses +1
-      if guesses = 3
+    svar = gets.chomp
+      if svar1 == easy
         running = false
-  elsif input == medium
-    puts "guess a number betwen 1 and 50"
-    gets.chomp = svar2
-    if svar2 == medium
-      input = false
-    else
-      gesses +1
-  else input == hard
-    puts "guess a number betwen 1 and 100"
+        p "congrats"
+      else
+        p "try again"
+        tries +1
+        if tries == 3
+          running = false
+    elsif input == medium
+      puts "guess a number betwen 1 and 50"
+       svar2 = gets.chomp
+          if svar2 == medium
+            p "congrats"
+            running = false
+          else
+            tries +1
+            if tries == 5
+              input = false
+        else input == hard
+          puts "guess a number betwen 1 and 100"
+          svar3 = gets.chomp
+              if svar3 == hard
+                p "congrats"
+                running =false
+              else
+                tries +1
+                if tries == 5
+                  running = false
 
 
-
+                end
+              end
+            end
+          end
+        end
+      end
+    end
   end
-end
